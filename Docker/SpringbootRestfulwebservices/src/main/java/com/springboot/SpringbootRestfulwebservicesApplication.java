bf8636def431c6153c808c76652f5568167b19f1
pradeep <k.pradeep0491@gmail.com>
2024-06-15 12:46:16 +0530

    changes

---
+++ Docker/SpringbootRestfulwebservices/src/main/java/com/springboot/SpringbootRestfulwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulwebservicesApplication.class, args);
+	}
+
+}
