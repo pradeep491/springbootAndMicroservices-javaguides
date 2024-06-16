8b0129270c0e64afdf1258e549be48389ca3c673
pradeep <k.pradeep0491@gmail.com>
2024-06-16 11:18:39 +0530

    changes

---
+++ SpringBoot/SpringBootRestAPI-Actuator/src/main/java/com/test/springboot/controller/UserController.java
@@ -0,0 +1,71 @@
+package com.test.springboot.controller;
+
+import com.test.springboot.dto.UserDTO;
+import com.test.springboot.entity.User;
+import com.test.springboot.exception.ErrorDetails;
+import com.test.springboot.exception.ResourceNotFoundException;
+import com.test.springboot.mapper.UserMapper;
+import com.test.springboot.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService service;
+
+    //build create user rest api
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO user) {
+        UserDTO savedUser = service.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    //Build user find by id
+    //http://localhost:8080/api/users/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        UserDTO dto = service.getUserById(id);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    //http://localhost:8080/api/users/allUsers
+    @GetMapping("/allUsers")
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> users = service.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    //update Rest API
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable("id") Long userId, @RequestBody @Valid UserDTO user) {
+        user.setId(userId);
+        UserDTO updateduser = service.updateUser(user);
+        return new ResponseEntity<>(updateduser, HttpStatus.OK);
+    }
+
+    //Build Delete REST API
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
+        return new ResponseEntity<>("User Successfully deleted...!", HttpStatus.OK);
+    }
+
+    /*@ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                        WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                                                     exception.getMessage(),
+                                                     request.getDescription(false),
+                                            "User Not FOund");
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }*/
+}
