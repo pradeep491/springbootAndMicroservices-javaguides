2fc2f7e583eebbb78326d5266757bf509309df40
pradeep <k.pradeep0491@gmail.com>
2024-06-22 21:59:26 +0530

    changes

---
+++ SpringBoot/SpringBootTransactionDemo/src/main/java/com/test/springboot/SpringBootTransactionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransactionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTransactionDemoApplication.class, args);
+	}
+
+}
